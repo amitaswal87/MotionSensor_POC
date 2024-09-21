50ba877084458e7ff902cbe354305033aea852b0
Amit <amitaswal87@gmail.com>
2024-09-21 13:03:15 +0200

    Initial commit. Project and Readme file created

---
+++ MotionSensor_POC/MotionSensor_POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotionSensor_POC
+//
+//  Created by apple  on 21/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
